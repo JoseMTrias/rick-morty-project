786f9bcfdabff3054504afff3191400aff986053
JoseMTrias <jmgarciatrias@gmail.com>
2023-02-06 13:02:06 +0100

    finishing the function and forEach

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -9,14 +11,19 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+let maxPage = 1;
+let page = 1;
+let searchQuery = "";
 
 async function fetchCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
-  const charactersApi = await response.json();
-  console.log(charactersApi);
-  return charactersApi;
+  const response = await fetch("https://rickandmortyapi.com/api/character/");
+  const data = await response.json();
+  cardContainer.innerHTML = "";
+
+  const characterCard = data.results;
+  characterCard.forEach((character) => {
+    const li = createCharacterCard(character);
+    cardContainer.append(li);
+  });
 }
 fetchCharacters();
