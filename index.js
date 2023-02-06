651b3e1e9cd8974802cff9c1b0112e70143e8151
JoseMTrias <jmgarciatrias@gmail.com>
2023-02-06 15:16:54 +0100

    finishing pagination

--- index.js
+++ index.js
@@ -11,21 +11,40 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-let maxPage = 1;
+let maxPage = 42;
 let page = 1;
 let searchQuery = "";
 
 async function fetchCharacters() {
   const response = await fetch(
-    "https://rickandmortyapi.com/api/character/?page=<pageIndex>"
+    `https://rickandmortyapi.com/api/character/?page=${page}`
   );
   const data = await response.json();
   cardContainer.innerHTML = "";
-
   const characterCard = data.results;
   characterCard.forEach((character) => {
     const li = createCharacterCard(character);
     cardContainer.append(li);
   });
 }
+
+nextButton.addEventListener("click", () => {
+  if (page <= 41) {
+    page++;
+    fetchCharacters();
+    pagination.innerHTML = `${page} / ${maxPage}`;
+  } else {
+    pagination.innerHTML = `${page} / 42`;
+  }
+});
+
+prevButton.addEventListener("click", () => {
+  if (page >= 2) {
+    page--;
+    fetchCharacters();
+    pagination.innerHTML = `${page} / ${maxPage}`;
+  } else {
+    pagination.innerHTML = `${page} / ${maxPage}`;
+  }
+});
 fetchCharacters();
